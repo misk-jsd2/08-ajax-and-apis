6bd652ac06d3136463fcbd847f0f218d2a8fda92
yazeedhathal <abohathal1@gmail.com>
2019-04-02 00:31:51 +0300

    08 lesson

--- starter-code/jquery_ajax/main.js
+++ starter-code/jquery_ajax/main.js
@@ -11,8 +11,39 @@
 
 'use strict';
 (function() {
-  var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+  var weatherUrl = "http://api.openweathermap.org/data/2.5/weather";
   var apiKey = "";
 
-  });
+  var appid = "4452be3c247c2a34b8f52b1211951825";
+  var userinput = document.getElementById('search');
+  var botton = document.getElementById('search-button')
+
+botton.onclick = function(event) {
+     event.preventDefault();
+$.ajax({
+     url:weatherUrl,
+	 type: "GET",
+  data:{
+    q: userinput.value,
+    appid: appid
+  },
+  success: function(response){
+    console.log(response.main);
+     
+     var kelvinn = response.main.temp  - 273;
+
+     var vehrnhite =  Math.floor(kelvinn * (9/5) + 32)
+
+    var newThingLi = document.createElement('p');
+    var newThingText =  document.createTextNode( 'The temperature today in '+ userinput.value +' is '+vehrnhite +' '+'Fahrenheit and the humidity is '+ response.main.humidity) ;
+    newThingLi.appendChild(newThingText);
+    document.body.appendChild(newThingLi);
+    
+  },
+  error: function(e){
+  	console.log(e);
+  }
+})
+
+}
 })();
