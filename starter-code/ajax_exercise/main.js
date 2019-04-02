7f2c42becc8091b2851addd9aef0efe954976ffa
mohammedsanad <mohammedalsanad1933@gmail.com>
2019-04-02 18:48:58 +0300

    Mohammed ALsanad HomwWork

--- starter-code/ajax_exercise/main.js
+++ starter-code/ajax_exercise/main.js
@@ -13,5 +13,30 @@ success and error scenarios.
 
 'use strict';
 (function() {
+
+  var httpRequest = new XMLHttpRequest();
+
+  httpRequest.onreadystatechange = responseMethod;
+
+  function responseMethod() {
+    console.log(httpRequest);
+    // Check if our state is "DONE"
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      // If our request was successful we get a return code/status of 200
+      if (httpRequest.status === 200) {
+        // This is where we update our UI accordingly. Our data is available to us through the responseText parameter
+        console.log(httpRequest.responseText);
+      } else {
+        // This is the scenario that there was an error with our request
+        console.log('There was a problem with the request.');
+      }
+    }
+  }
+    httpRequest.open('GET','http://data.consumerfinance.gov/api/views.json');
+ 
+
+  // The send method takes an optional parameter. If our API request allows additional parameters or JSON objects to be passed through (primarily through POST requests), we pass them in the send method.
+    httpRequest.send();
+
   // Alternate data source: https://data.cityofnewyork.us/api/views/jb7j-dtam/rows.json?accessType=DOWNLOAD
 })();
--- starter-code/jquery_ajax/main.js
+++ starter-code/jquery_ajax/main.js
@@ -12,7 +12,20 @@
 'use strict';
 (function() {
   var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
-  var apiKey = "";
+  var userinput = "city";
+  var appid = "43daa34bfbf43a4d05f3d2b170269cd1";
+  $.ajax ({
+    url : weatherUrl ,
+    type : 'GET',
+    data :  {
+      q: userInput,
+      appid: appid
 
-  });
+     },
+
+      // Work with the response
+      success: function( response ) {
+          console.log( response ); // server response
+      }
+  })
 })();
