90acf1f192b4f110ede944728a2fbf05e753781d
Ibrahim Alduayji <ialduayji@gmail.com>
2019-04-02 17:55:53 +0300

    alduayji

--- starter-code/jquery_ajax/main.js
+++ starter-code/jquery_ajax/main.js
@@ -9,10 +9,59 @@
 
 */
 
-'use strict';
+// 'use strict'; 
 (function() {
-  var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+  // take the user input to be desplyed on screen. H/W
+  var weatherUrl = "http://api.openweathermap.org/data/2.5/weather";
   var apiKey = "";
-
-  });
+  
+  var appId = '43daa34bfbf43a4d05f3d2b170269cd1'
+  
+  var userInput = document.getElementById('user');
+   
+  
+  var click1 = document.getElementById('submit');
+  click1.onclick = function(event){
+    event.preventDefault();
+    var userText = userInput.value;
+  $.ajax({
+    url: weatherUrl,
+    type: 'GET',
+    data:{
+      q: userText,
+      appid: appId
+    },
+    success: function(response){
+      var humidity = (response.main.humidity);
+        
+        var creatP = document.createElement('P');
+        document.body.appendChild(creatP);
+        creatP.innerHTML = (`the humidity in ${userText} is ${humidity}`);
+        
+        
+        
+      }
+    })
+  
+  }
+    
+    
+  
 })();
+  
+
+// var userInput = 'r2'
+
+// var starrWarsUrl = 'https://swapi.co/api/people/?search=r2'
+
+// $.ajax({
+//  url:starrWarsUrl,
+//  data :{
+//    search: userInput
+//  }
+
+// })
+
+//   // $.get('https://data.cityofnewyork.us/api/views/jb7j-dtam/rows.json?accessType=DOWNLOAD', function(response){
+//   //   console.log(response.data)
+//   // })
