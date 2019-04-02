ee455a6c3ca5c004be1a9ff0ff99c37f4182a76f
Fahad Alessa <Fahad.alisa@gmail.com>
2019-04-02 17:53:07 +0300

    Fahad Alessa

--- starter-code/ajax_exercise/main.js
+++ starter-code/ajax_exercise/main.js
@@ -13,5 +13,40 @@ success and error scenarios.
 
 'use strict';
 (function() {
+
+
+	var httpRequest = new XMLHttpRequest();
+	
+	httpRequest.onreadystatechange = responseMethod;
+
+	function responseMethod() {
+		if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      // If our request was successful we get a return code/status of 200
+      		if (httpRequest.status === 200) {
+        // This is where we update our UI accordingly. Our data is available to us through the responseText parameter
+        		console.log(httpRequest.responseText);
+      		} 
+      		else {
+        // This is the scenario that there was an error with our request
+        		console.log('There was a problem with the request.');
+      		}
+   		 }
+
+	}
+
+	var button = document.getElementById('getCustomDataButton');
+    button.onclick = function(event) {
+     	event.preventDefault();
+		httpRequest.open('GET', 'https://data.cityofnewyork.us/api/views/jb7j-dtam/rows.json?accessType=DOWNLOAD');
+		httpRequest.send();
+	}
+	
+	var button2 = document.getElementById('getDataButton');
+	button2.onclick = function(event) {
+     	event.preventDefault();
+		httpRequest.open('GET', 'http://data.consumerfinance.gov/api/views.json');
+		httpRequest.send();
+	}
+
   // Alternate data source: https://data.cityofnewyork.us/api/views/jb7j-dtam/rows.json?accessType=DOWNLOAD
 })();
--- starter-code/jquery_ajax/main.js
+++ starter-code/jquery_ajax/main.js
@@ -1,18 +1,37 @@
-/*
 
-- Sign up for openweathermap.org and generate an API key.
-- User either $.ajax or $.get to pull weather current data .
-  for Washington DC (hint: http://api.openweathermap.org/data/2.5/weather?q=...).
-- Print the temperature in console.
-- Bonus 1: add a form prompting user for the city and state.
-- Bonus 2: convert answer from kelvin to fahrenheit.
+var button = document.getElementById('new-weather-button');
+button.onclick = function(event) {
+	event.preventDefault();
+    var userInput = document.getElementById("new-weather").value;
 
-*/
+	'use strict';
+	(function() {
+  		var weatherUrl = "http://api.openweathermap.org/data/2.5/weather";
+  		var appid = "4144587869c5311f463fce316e263bc0";
+  		var units = 'metric';
 
-'use strict';
-(function() {
-  var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
-  var apiKey = "";
-
-  });
-})();
+  		$.ajax({
+  			url: weatherUrl,
+  			type: 'GET',
+  			data: {
+	  			q: userInput,
+  				appid: appid,
+  				units: units
+  			},
+  			success: function( response ) {
+	    		console.log( response ); // server response
+  			}
+  		})
+  		.done(function(response) {
+  			var temp = response.main.temp;
+  			var wind = response.wind.speed;
+  			var humidity = response.main.humidity
+  			var x = $('<p>').text("Temperature: " + temp + "°C, ")
+  			var y = $('<p>').text("Wind Speed: " + wind + ", ")
+  			var z = $('<p>').text("Humidity: " + humidity)
+  			$('#monitor_data').append(x);
+  			$('#monitor_data').append(y);
+  			$('#monitor_data').append(z);
+		});
+	})();
+}
