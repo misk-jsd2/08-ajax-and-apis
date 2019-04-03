543fdb819f3d83e4fca49a6c059ca8ddfa0ff6ac
bashayeralghafis <bashayeralghafis@gmail.com>
2019-04-04 02:02:18 +0300

    HomeWork Weather

--- starter-code/ajax_exercise/main.js
+++ starter-code/ajax_exercise/main.js
@@ -11,7 +11,55 @@ click of "Get Custom Data". Interact with an API of your choice and handle both
 success and error scenarios.
 */
 
+
+
+  // Alternate data source: https://data.cityofnewyork.us/api/views/jb7j-dtam/rows.json?accessType=DOWNLOAD
+
 'use strict';
 (function() {
-  // Alternate data source: https://data.cityofnewyork.us/api/views/jb7j-dtam/rows.json?accessType=DOWNLOAD
-})();
+
+ // Create instance of XMLHTTPRequest
+ var httpRequest = new XMLHttpRequest();
+//
+
+
+ // Set a custom function to handle the request
+ httpRequest.onreadystatechange = responseMethod;
+
+ function responseMethod() {//decler 
+   // Request logic
+ 
+
+ // Alternative method:
+ // httpRequest.onreadystatechange = function() {
+ //   
+ // }
+ 
+   // Check if our state is "DONE"
+  if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    // If our request was successful we get a return code/status of 200
+    if (httpRequest.status === 200) { //200 it is mean ok
+      // This is where we update our UI accordingly. Our data is available to us through the responseText parameter
+      console.log(httpRequest.responseText);
+    } else {
+      // This is the scenario that there was an error with our request
+      console.log('There was a problem with the request.');
+    }
+  }
+}
+ 
+//  httpRequest.open('GET', 'http://data.consumerfinance.gov/api/views.json');
+
+// httpRequest.send();
+
+var $button = $('#getCustomDataButton');//
+  $button.on('click', function(event) {//
+    event.preventDefault();//
+  httpRequest.open('GET', 'https://data.cityofnewyork.us/api/views/jb7j-dtam/rows.json?accessType=DOWNLOAD');
+  httpRequest.send();
+})})();
+
+document.getElementById('getCustomDataButton').addEventListener("click", request);//add
+
+
+
--- starter-code/jquery_ajax/main.js
+++ starter-code/jquery_ajax/main.js
@@ -9,10 +9,100 @@
 
 */
 
+ 
 'use strict';
 (function() {
-  var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
-  var apiKey = "";
 
-  });
+  var weatherUrl = "http://api.openweathermap.org/data/2.5/weather";
+  var appid = 'b2fc3ff73ef87cee9c99ad9945fd7f80';
+  
+  var $userinput = $('#user_input');
+  var $button = $('#Button');
+  $button.on('click', function(event) {
+     event.preventDefault();
+     var userInput = $userinput.val();
+ 
+
+  $.ajax({
+    url: weatherUrl,
+    type: 'GET',
+    data: {
+      q : userInput,
+      appid: appid,
+    },
+    success: function(response){
+      console.log(response)
+
+
+ 
+      var $cityname = response.name;
+      var $CityName = $('#CityName');
+      $CityName.text($cityname);
+ 
+      var $temp = response.main.temp;
+      var $Temp = $('#Temp');
+       var $Tempc= ($temp - 273.15);      
+      $Temp.text('Temperature : ' + $Tempc +'C');
+ 
+
+     }
+
+  })
+
+  })
 })();
+
+
+
+
+
+
+
+
+//   'use strict';
+// (function() {
+//   var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?";
+//   var userInput = 'London';
+//   var appid = 'b2fc3ff73ef87cee9c99ad9945fd7f80';
+
+
+//   $.ajax({
+//     url: weatherUrl,
+//     type:'GET',
+//     data: {
+//       q : userInput,
+//       appid : appid,
+//     },
+//     success: function (response) {
+//       console.log(response);
+
+//     }
+//   })
+// })();
+
+
+//-------------------------------
+
+
+  // var starWarsURL = 'https://swapi.co/api/people/?search=r2'
+   
+//   $.ajax({
+//    url: starWarsURL,
+//    type: 'GET',
+//    data:{
+//     search: userInput
+//    // https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22
+
+//   }, success: function(response){
+//     console.log(response)
+//   }
+
+//   });
+
+
+// $.get('https://data.cityofnewyork.us/api/views/jb7j-dtam/rows.json?accessType=DOWNLOAD', function(response){
+//   console.log(response.data)
+// })
+
+
+// })();
