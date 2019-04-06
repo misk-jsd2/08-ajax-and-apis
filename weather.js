ff313289cab124cb9e18451dc232210b87fb1784
nourah08 <48182464+nourah08@users.noreply.github.com>
2019-04-06 14:20:32 +0300

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,19 +1,26 @@
-var HttpRequest = new XMLHttpRequest();
-​
-  HttpRequest.onreadystatechange = responseM;
-  function responseM(){
-​
-	  if (HttpRequest.readyState === XMLHttpRequest.DONE) {
-	   if (HttpRequest.status === 200) {
-	    console.log(HttpRequest.response);
-	   } else {
-	    console.log('request error');
-	   }
-​
-	 }
-	 HttpRequest.open('GET', ' http://api.openweathermap.org/data/2.5/weather');
-	 HttpRequest.send();
-​
-	};
-​
-  })();
+
+'use strict';
+(function() {
+  var HttpRequest = new XMLHttpRequest();
+
+   HttpRequest.onreadystatechange = response;
+   function response(){
+	}}();
+
+'use strict';
+(function getweather () {
+  var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+  var apiKey = "";
+	   if (HttpRequest.readyState === XMLHttpRequest.DONE) {
+	     if (HttpRequest.status === 200) {
+	       console.log(HttpRequest.response);
+	     } else {
+	       console.log('request error');
+	     }
+
+	  }
+    document.getElementById('city').addEventListener("click", request);
+	  HttpRequest.open('GET', ' http://api.openweathermap.org/data/2.5/weather');
+	  HttpRequest.send();
+
+	}) ();
