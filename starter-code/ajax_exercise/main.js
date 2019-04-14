aa7900a8cff98a626960d02a01d1be79c81ad02e
Oz3Oz3 <oz_3@ymail.com>
2019-04-14 10:51:46 +0300

    Abdulaziz homework, updated

--- starter-code/ajax_exercise/main.js
+++ starter-code/ajax_exercise/main.js
@@ -1,40 +1,46 @@
 
 var button = document.getElementById('weatherButton');
-button.onclick = (function(event) {
-	event.preventDefault();
-    var userInput = document.getElementById('weatherText').value;
+button.onclick = (function(GetTemp) {
+    var usrInput = document.getElementById('weatherText').value;
 
-
-	'use strict';
-	(function() {
-  		var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather';
-  		var apiKey = '4144587869c5311f463fce316e263bc0';
-      var unit = 'metric';
+		'use strict';
+		(function() {
+			var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+			var apiKey = "4144587869c5311f463fce316e263bc0";
+			var unit = 'metric';
       var lang = 'ar';
-
-  });
-  		$.ajax({
-  			url: weatherUrl,
-  			type: 'GET',
-  			data: {
-	  			q: userInput,
-  				appid: apiKey,
-          units: unit,
-          lang: lang,
-  			},
-  			success: function( response ) {
-	    		console.log( response ); // server response
-  			}
-  		})
-  		.done(function(response) {
-  			var temp = response.main.temp;
-  			var desc = response.wind.speed;
-  			var humidity = response.main.humidity
-  			var x = $('<p>').text("Temperature: " + temp + "°C, ")
-  			var y = $('<p>').text("Wind Speed: " + wind + ", ")
-  			var z = $('<p>').text("Humidity: " + humidity)
-  			$('#Data').append(x);
-  			$('#Data').append(y);
-  			$('#Data').append(z);
-    });
-  });
+		
+			getWeather(usrInput);
+		
+			$('#GetTemp').on('click', function(event) {
+				getWeather($('#usrInput').val());
+			});
+		
+			function getWeather(usrInput) {
+				$.ajax({
+						url: weatherUrl + usrInput + ',' + '&appid=' + apiKey,
+		
+						// Work with the response
+						success: function( response ) {
+								updateUISuccess(response.main.temp, usrInput);
+						},
+		
+						error: function() {
+							updateUIError();
+						}
+				});
+			}
+		
+			function updateUISuccess(temp, usrInput) {
+				$('#usrInput').val('');
+				$('#location').html(usrInput);
+				console.log(temp);
+				$('#temp').html(temp);
+			}
+		
+			function updateUIError() {
+				alert("There was an error getting weather data :(");
+			}
+		
+		})();
+		
